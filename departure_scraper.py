d7452065c7f83a4cbb65032a7f4f518374fe48b9
Rainer Mesi <rainer.mesi@gmail.com>
2024-07-30 09:10:43 +0300

    Create departure_scraper.py
    Skeleton for departure scraping script

---
+++ departure_scraper.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://airport.ee/lennuinfo/"
+
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "upgrade-insecure-requests": "1",
+    "referer": "https://www.google.com/"
+}
+
+response = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+flights_list_items = soup.find_all('li', class_='flights-list__item departures-today')
+
+# Extract the required data from each li element
+for li in flights_list_items:
+    flight_numbers = li.find('span', class_='card-flight__flight-numbers')
+    service_providers = li.find('span', class_='card-flight__service-providers')
+    flight_time = li.find('span', class_='card-flight__time')
+    depart_time = li.find('em', class_='card-flight__tag card-flight__tag--departed')
+    try:
+        flight_title = li.find('div', class_='card-flight__content').find('h2', class_='card-flight__title')
+    except:
+        flight_title = 'N/A'
+    flight_numbers_text = flight_numbers.get_text(strip=True) if flight_numbers else 'N/A'
+    flight_time_text = flight_time.get_text(strip=True) if flight_time else 'N/A'
+    depart_time_text = depart_time.get_text(strip=True) if depart_time else 'N/A'
+    service_providers_text = service_providers.get_text(strip=True) if service_providers else 'N/A'
+    try:
+        flight_title_text = flight_title.get_text(strip=True)
+    except:
+        flight_title_text = 'N/A'
+
+    print(f"Flight Numbers: {flight_numbers_text}")
+    print(f"Flight Time: {flight_time_text}")
+    print(f"Depart Time: {depart_time_text}")
+    print(f"Service Providers: {service_providers_text}")
+    print(f"Flight Title: {flight_title_text}")
+    print("----------")
